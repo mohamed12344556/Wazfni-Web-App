ba65fc2f44bb7ee565f99f6c0f780cb2c72a4b1d
mohamed12344556 <ma5123@fayoum.edu.eg>
2024-11-08 23:06:52 +0200

    mytest branch

--- lib/mobile/ui/home_page.dart
+++ lib/mobile/ui/home_page.dart
@@ -4,28 +4,87 @@ import 'package:wazafny_app/mobile/ui/contact_page.dart';
 import 'package:wazafny_app/mobile/ui/profile_page.dart';
 import 'package:wazafny_app/mobile/ui/recent_work_page.dart';
 import 'package:wazafny_app/mobile/ui/test_imonials_page.dart';
-
 import '../../core/themes/app_colors.dart';
-
 import '../widgets/build_end_drawer.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final ScrollController _scrollController = ScrollController();
+
+  final GlobalKey _homeKey = GlobalKey();
+  final GlobalKey _caseStudiesKey = GlobalKey();
+  final GlobalKey _testimonialsKey = GlobalKey();
+  final GlobalKey _recentWorkKey = GlobalKey();
+  final GlobalKey _contactKey = GlobalKey();
+
+  void scrollToSection(GlobalKey key) {
+    final context = key.currentContext;
+    if (context != null) {
+      Scrollable.ensureVisible(
+        context,
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.easeInOut,
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.pureBlack,
       appBar: AppBar(),
-      endDrawer: const BuildEndDrawer(),
+      endDrawer: BuildEndDrawer(
+        onSectionSelected: (section) {
+          switch (section) {
+            case 'home':
+              scrollToSection(_homeKey);
+              break;
+            case 'caseStudies':
+              scrollToSection(_caseStudiesKey);
+              break;
+            case 'testimonials':
+              scrollToSection(_testimonialsKey);
+              break;
+            case 'recentWork':
+              scrollToSection(_recentWorkKey);
+              break;
+            case 'contact':
+              scrollToSection(_contactKey);
+              break;
+          }
+          Navigator.of(context).pop();
+        },
+      ),
       body: SingleChildScrollView(
+        controller: _scrollController,
         child: Column(
           children: [
-            const ProfilePage(),
-            const CaseStudies(),
-            TestImonialsPage(),
-            const RecentWorkPage(),
-            const ContactPage(),
+            Container(
+              key: _homeKey,
+              child: const ProfilePage(),
+            ),
+            Container(
+              key: _caseStudiesKey,
+              child: const CaseStudies(),
+            ),
+            Container(
+              key: _testimonialsKey,
+              child: TestImonialsPage(),
+            ),
+            Container(
+              key: _recentWorkKey,
+              child: const RecentWorkPage(),
+            ),
+            Container(
+              key: _contactKey,
+              child: const ContactPage(),
+            ),
           ],
         ),
       ),
