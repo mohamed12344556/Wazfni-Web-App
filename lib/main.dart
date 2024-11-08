b3a2d8646274f8ff0843b0fed3fa65d609deb71b
mohamed12344556 <ma5123@fayoum.edu.eg>
2024-11-08 23:24:02 +0200

    add DevicePreview

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,28 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:wazafny_app/core/themes/app_colors.dart';
 import 'package:wazafny_app/mobile/ui/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      // useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       theme: ThemeData(
         scaffoldBackgroundColor: AppColors.white,
         appBarTheme: const AppBarTheme(backgroundColor: AppColors.pureBlack),
